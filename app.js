07466b1e97f86acf14ab5496db15f391a73b6391
khoiduong-freec <khoiduong@freec.asia>
2026-06-08 15:50:51 +0700

    Exclude user 123 from CSharp weekly report

--- app.js
+++ app.js
@@ -572,12 +572,11 @@ async function fetchReportLists() {
   const ranges = getReportDateRanges();
   const reportStartDate = getSelectedReportStartDate();
   validateReportStartDate(reportStartDate, ranges.current.monday);
-  const prevCsharpAssigneeIds = [106, 94, 99];
-  const csharpAssigneeIds = [106, 94, 99, 123];
+  const csharpAssigneeIds = [106, 94, 99];
   const webAssigneeIds = [123];
 
   const [prevCsharp, prevWeb, currentCsharp, currentWeb] = await Promise.all([
-    fetchReportList({ assigneeIds: prevCsharpAssigneeIds, range: ranges.previous, dueDateFrom: reportStartDate }),
+    fetchReportList({ assigneeIds: csharpAssigneeIds, range: ranges.previous, dueDateFrom: reportStartDate }),
     fetchReportList({ assigneeIds: webAssigneeIds, range: ranges.previous, dueDateFrom: reportStartDate }),
     fetchReportList({ assigneeIds: csharpAssigneeIds, range: ranges.current }),
     fetchReportList({ assigneeIds: webAssigneeIds, range: ranges.current }),
